4e702e00747ca23a6a7b304ac12ebbda0558323c
MariaCosmos <cosmos.maria2003@gmail.com>
2022-10-17 11:45:23 -0300

    Exercicios extra - Imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import Garagem from "./Componentes/Garagem";
 import { GlobalStyled } from "./globalStyled";
+import { useState } from "react";
 
 export default function App() {
   const nome = "Turma";
+  const [nomeEstado, setNomeEstado] = useState("Cosmos")
+
+  function mudarNome(){
+    setNomeEstado("Cosmico °0°")
+  }
+
   function apresentaGaragem() {
     alert(`Boas vindas à garagem de ${nome}`);
   }
@@ -11,7 +18,7 @@ export default function App() {
     <div className="App">
       <GlobalStyled />
 
-      <Garagem nome={nome} mensagemAprentacao={apresentaGaragem} />
+      <Garagem nome={nomeEstado} mensagemAprentacao={mudarNome} />
     </div>
   );
 }
--- src/Componentes/Garagem.js
+++ src/Componentes/Garagem.js
@@ -1,31 +1,68 @@
 import Carro from "./Carro";
 import { Button, EstilizacaoDaGaragem } from "./styledGaragem";
+import { useState } from "react";
 
 function Garagem(props) {
+  const [automovel1, setAutomovel1] = useState({
+    cor:"Magenta", 
+    ano: 2077, 
+    flex:"não"
+  })
+  const [automovel2, setAutomovel2] = useState({
+    cor:"Roxo", 
+    ano: 2053, 
+    flex:"sim"
+  })
+  const [automovel3, setAutomovel3] = useState({
+    cor:"Bege", 
+    ano: 2037, 
+    flex:"sim"
+  })
+  const [automovel4, setAutomovel4] = useState({
+    cor:"Verde", 
+    ano: 2033, 
+    flex:"não"
+  })
+
+  function mudarCorCarro(){
+    setAutomovel1({...automovel1, cor:"Amarelo"})
+    setAutomovel2({...automovel2, cor:"Vermelho"})
+    setAutomovel3({...automovel3, cor:"Lilas"})
+    setAutomovel4({...automovel4, cor:"Cinza"})
+
+  }
+
+
   return (
     <EstilizacaoDaGaragem>
-      <h1>Garagem da {props.nome}</h1>
-      <Button onClick={props.mensagemAprentacao}>Mensagem</Button>
+      <h1>Garagem do {props.nome}</h1>
+      <button onClick={props.mensagemAprentacao}>Mensagem</button>
+      <button onClick={mudarCorCarro}>Mudar as cores</button>
       <Carro
         adicionadoPor={props.nome}
-        cor={"Vermelho"}
-        ano={2022}
-        flex={"true"}
+        cor={automovel1.cor}
+        ano={automovel1.ano}
+        flex={automovel1.flex}
       />
       <Carro
         adicionadoPor={props.nome}
-        cor={"Verde"}
-        ano={2020}
-        flex={"false"}
+        cor={automovel2.cor}
+        ano={automovel2.ano}
+        flex={automovel2.flex}
       />
       <Carro
         adicionadoPor={props.nome}
-        cor={"Amarelo"}
-        ano={2009}
-        flex={"false"}
+        cor={automovel3.cor}
+        ano={automovel3.ano}
+        flex={automovel3.flex}
       />
-      <Carro adicionadoPor={props.nome} cor={"Rosa"} ano={2022} flex={"true"} />
-    </EstilizacaoDaGaragem>
+      <Carro 
+      adicionadoPor={props.nome} 
+      cor={automovel4.cor} 
+      ano={automovel4.ano}
+      flex={automovel4.flex} 
+      />
+   </EstilizacaoDaGaragem>
   );
 }
 
